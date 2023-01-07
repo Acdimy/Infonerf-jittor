21bf032438aa7fb5e043d959bb9994660361345f
Acdimy <49313774+Acdimy@users.noreply.github.com>
2023-01-07 23:35:20 +0800

    version 0

---
+++ loss.py
@@ -0,0 +1,41 @@
+import jittor as jt
+
+class EntropyLoss:
+    def __init__(self, args):
+        super(EntropyLoss, self).__init__()
+        self.N_samples = args.N_rand
+        self.type_ = args.entropy_type 
+        self.threshold = args.entropy_acc_threshold
+        self.computing_entropy_all = args.computing_entropy_all
+        self.smoothing = args.smoothing
+        self.computing_ignore_smoothing = args.entropy_ignore_smoothing
+        self.entropy_log_scaling = args.entropy_log_scaling
+        self.N_entropy = args.N_entropy 
+        
+        if self.N_entropy ==0:
+            self.computing_entropy_all = True
+
+    def ray_zvals(self, sigma, acc):
+        # if self.smoothing and self.computing_ignore_smoothing:
+        #     N_smooth = sigma.size(0)//2
+        #     acc = acc[:N_smooth]
+        #     sigma = sigma[:N_smooth]
+        if not self.computing_entropy_all:
+            acc = acc[self.N_samples:]
+            sigma = sigma[self.N_samples:]
+        ray_prob = sigma / (jt.sum(sigma,-1).unsqueeze(-1)+1e-10)
+        entropy_ray = self.entropy(ray_prob)
+        entropy_ray_loss = jt.sum(entropy_ray, -1)
+        
+        # masking no hitting poisition?
+        mask = (acc>self.threshold).detach()
+        entropy_ray_loss*= mask
+        # if self.entropy_log_scaling:
+        #     return jt.log(jt.mean(entropy_ray_loss) + 1e-10)
+        return jt.mean(entropy_ray_loss)
+    
+    def entropy(self, prob):
+        if self.type_ == 'log2': # Default
+            return -1*prob*jt.log2(prob+1e-10)
+        elif self.type_ == '1-p':
+            return prob*jt.log2(1-prob)
---
+++ utils.py
@@ -0,0 +1,157 @@
+import jittor as jt
+import numpy as np
+import imageio, tqdm, os
+
+img2mse = lambda x, y : jt.mean((x - y) ** 2)
+mse2psnr = lambda x : -10. * jt.log(x) / jt.log(jt.float32([10.]))
+to8b = lambda x : (255. * np.clip(x, 0, 1)).astype(np.uint8) # to save image
+
+def img2psnr_redefine(x,y):
+    '''
+    we redefine the PSNR function,
+    [previous]
+    average MSE -> PSNR(average MSE)
+    
+    [new]
+    average PSNR(each image pair)
+    '''
+    image_num = x.size(0)
+    mses = ((x-y)**2).reshape(image_num, -1).mean(-1)
+    
+    psnrs = [mse2psnr(mse) for mse in mses]
+    psnr = jt.stack(psnrs).mean()
+    return psnr
+
+def get_rays(H, W, focal, c2w):
+    # pytorch's meshgrid has indexing='ij'
+    i, j = jt.meshgrid(jt.linspace(0, W-1, W), jt.linspace(0, H-1, H))
+    i = i.t()
+    j = j.t()
+    dirs = jt.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -jt.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = jt.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = c2w[:3,-1].expand(rays_d.shape)
+    return rays_o, rays_d
+
+def ndc_rays(H, W, focal, near, rays_o, rays_d):
+    # Shift ray origins to near plane
+    t = -(near + rays_o[...,2]) / rays_d[...,2]
+    rays_o = rays_o + t[...,None] * rays_d
+    
+    # Projection
+    o0 = -1./(W/(2.*focal)) * rays_o[...,0] / rays_o[...,2]
+    o1 = -1./(H/(2.*focal)) * rays_o[...,1] / rays_o[...,2]
+    o2 = 1. + 2. * near / rays_o[...,2]
+
+    d0 = -1./(W/(2.*focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
+    d1 = -1./(H/(2.*focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
+    d2 = -2. * near / rays_o[...,2]
+    
+    rays_o = jt.stack([o0,o1,o2], -1)
+    rays_d = jt.stack([d0,d1,d2], -1)
+    
+    return rays_o, rays_d
+
+def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=False, 
+               out_alpha=False, out_sigma=False, out_dist=False):
+    """Transforms model's predictions to semantically meaningful values.
+    Args:
+        raw: [num_rays, num_samples along ray, 4]. Prediction from model.
+        z_vals: [num_rays, num_samples along ray]. Integration time.
+        rays_d: [num_rays, 3]. Direction of each ray.
+    Returns:
+        rgb_map: [num_rays, 3]. Estimated RGB color of a ray.
+        disp_map: [num_rays]. Disparity map. Inverse of depth map.
+        acc_map: [num_rays]. Sum of weights along each ray.
+        weights: [num_rays, num_samples]. Weights assigned to each sampled color.
+        depth_map: [num_rays]. Estimated distance to object.
+    """
+    raw2alpha = lambda raw, dists, act_fn=jt.nn.relu: 1.-jt.exp(-act_fn(raw)*dists)
+    
+    dists = z_vals[...,1:] - z_vals[...,:-1]
+    dists = jt.concat([dists, jt.float32([1e10]).expand(dists[...,:1].shape)], -1)  # [N_rays, N_samples]
+
+    dists = dists * jt.norm(rays_d[...,None,:], dim=-1)
+
+    rgb = jt.sigmoid(raw[...,:3])  # [N_rays, N_samples, 3]
+    noise = 0.
+    # if raw_noise_std > 0.:
+    #     noise = jt.randn(raw[...,3].shape) * raw_noise_std
+
+    #     # Overwrite randomly sampled data if pytest
+    #     if pytest:
+    #         np.random.seed(0)
+    #         noise = np.random.rand(*list(raw[...,3].shape)) * raw_noise_std
+    #         noise = jt.float32(noise)
+
+    alpha = raw2alpha(raw[...,3] + noise, dists)  # [N_rays, N_samples]
+    sigma = jt.nn.relu(raw[...,3]+noise)
+    # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
+    weights = alpha * jt.cumprod(jt.concat([jt.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1)[:, :-1]
+    rgb_map = jt.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
+
+    depth_map = jt.sum(weights * z_vals, -1)
+    disp_map = 1./jt.maximum(jt.float32(1e-10) * jt.ones_like(depth_map), depth_map / jt.sum(weights, -1))
+    acc_map = jt.sum(weights, -1)
+    
+    if white_bkgd:
+        rgb_map = rgb_map + (1.-acc_map[...,None])
+    
+    others = {}
+    if out_alpha or out_sigma or out_dist:
+        if out_alpha:
+            others['alpha'] = alpha
+        if out_sigma:
+            others['sigma'] = sigma
+        if out_dist:
+            others['dists'] = dists
+        return rgb_map, disp_map, acc_map, weights, depth_map, others # Default
+    return rgb_map, disp_map, acc_map, weights, depth_map
+
+
+# Hierarchical sampling (section 5.2)
+def sample_pdf(bins, weights, N_samples, det=False, pytest=False):
+    # Get pdf
+    weights = weights + 1e-5 # prevent nans
+    pdf = weights / jt.sum(weights, -1, keepdim=True)
+    cdf = jt.cumsum(pdf, -1)
+    cdf = jt.concat([jt.zeros_like(cdf[...,:1]), cdf], -1)  # (batch, len(bins))
+
+    # Take uniform samples
+    if det:
+        u = jt.linspace(0., 1., steps=N_samples)
+        u = u.expand(list(cdf.shape[:-1]) + [N_samples])
+    else:
+        u = jt.rand(list(cdf.shape[:-1]) + [N_samples])
+
+    # Pytest, overwrite u with numpy's fixed random numbersd
+    # if pytest:
+    #     np.random.seed(0)
+    #     new_shape = list(cdf.shape[:-1]) + [N_samples]
+    #     if det:
+    #         u = np.linspace(0., 1., N_samples)
+    #         u = np.broadcast_to(u, new_shape)
+    #     else:
+    #         u = np.random.rand(*new_shape)
+    #     u = torch.Tensor(u)
+
+    # Invert CDF
+    # u = u.contiguous() # !!!!!!
+    inds = jt.searchsorted(cdf, u, right=True)
+    below = jt.maximum(jt.zeros_like(inds-1), inds-1)
+    above = jt.minimum((cdf.shape[-1]-1) * jt.ones_like(inds), inds)
+    inds_g = jt.stack([below, above], -1)  # (batch, N_samples, 2)
+
+    # cdf_g = tf.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    # bins_g = tf.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
+    cdf_g = jt.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
+    bins_g = jt.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
+
+    denom = (cdf_g[...,1]-cdf_g[...,0])
+    denom = jt.where(denom<1e-5, jt.ones_like(denom), denom)
+    t = (u-cdf_g[...,0])/denom
+    samples = bins_g[...,0] + t * (bins_g[...,1]-bins_g[...,0])
+
+    return samples
